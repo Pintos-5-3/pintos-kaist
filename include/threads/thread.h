2da05104865b1043e84a3f9eb24dc40cfdd6d417
bada308 <203190@jnu.ac.kr>
2024-05-10 14:05:11 +0000

    Feat: thread 구조체에 wakeup_tick 변수 추가

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -92,6 +92,7 @@ struct thread
 	enum thread_status status; /* Thread state. */
 	char name[16];			   /* Name (for debugging purposes). */
 	int priority;			   /* Priority. */
+	int64_t wakeup_tick;	   /* wakeup 할 시간 저장 */
 
 	/* Shared between thread.c and synch.c. */
 	struct list_elem elem; /* List element. */
