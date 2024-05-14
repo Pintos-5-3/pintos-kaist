4934375c3216ebc9e462026cd6387a0bfab4d267
bada308 <203190@jnu.ac.kr>
2024-05-14 14:51:32 +0000

    Comment: sema, cond 동기화 TODO 추가

--- threads/synch.c
+++ threads/synch.c
@@ -66,6 +66,10 @@ void sema_down(struct semaphore *sema)
 	old_level = intr_disable();
 	while (sema->value == 0)
 	{
+		/**
+		 * TODO: 우선순위 순으로 waiters에 삽입
+		 * part: priority-sync
+		 */
 		list_push_back(&sema->waiters, &thread_current()->elem);
 		thread_block();
 	}
@@ -109,6 +113,10 @@ void sema_up(struct semaphore *sema)
 	ASSERT(sema != NULL);
 
 	old_level = intr_disable();
+	/**
+	 * TODO: waiters 정렬
+	 * part: priority-sync
+	 */
 	if (!list_empty(&sema->waiters))
 		thread_unblock(list_entry(list_pop_front(&sema->waiters),
 								  struct thread, elem));
@@ -284,6 +292,10 @@ void cond_wait(struct condition *cond, struct lock *lock)
 	ASSERT(lock_held_by_current_thread(lock));
 
 	sema_init(&waiter.semaphore, 0);
+	/**
+	 * TODO: 우선순위 순으로 waiters에 삽입
+	 * part: priority-sync
+	 */
 	list_push_back(&cond->waiters, &waiter.elem);
 	lock_release(lock);
 	sema_down(&waiter.semaphore);
@@ -304,6 +316,10 @@ void cond_signal(struct condition *cond, struct lock *lock UNUSED)
 	ASSERT(!intr_context());
 	ASSERT(lock_held_by_current_thread(lock));
 
+	/**
+	 * TODO: waiters 정렬
+	 * part: priority-sync
+	 */
 	if (!list_empty(&cond->waiters))
 		sema_up(&list_entry(list_pop_front(&cond->waiters),
 							struct semaphore_elem, elem)
