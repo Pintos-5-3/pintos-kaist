6dfd1fa8f3ccea78a4a3557f98b43b8ebde81a5c
bada308 <203190@jnu.ac.kr>
2024-05-23 05:32:46 +0000

    Feat: 시스템 콜 halt, exit, create, remove 구현 및 시스템 콜 핸들러 수정

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -7,8 +7,12 @@
 #include "userprog/gdt.h"
 #include "threads/flags.h"
 #include "intrinsic.h"
-/* TODO: 구현에 필요한 라이브러리 include */
+/* NOTE: [Part2] 구현에 필요한 라이브러리 include */
 #include "include/threads/init.h"
+#include "filesys/filesys.h"
+#include "lib/string.h"
+#include "lib/syscall-nr.h"
+#include "include/threads/malloc.h"
 
 void syscall_entry(void);
 void syscall_handler(struct intr_frame *);
@@ -26,6 +30,17 @@ void syscall_handler(struct intr_frame *);
 #define MSR_LSTAR 0xc0000082		/* Long mode SYSCALL target */
 #define MSR_SYSCALL_MASK 0xc0000084 /* Mask for the eflags */
 
+/* NOTE: [Part2] define */
+#define USER_AREA_STAR 0x8048000
+#define USER_AREA_END 0xc0000000
+
+void halt(void);
+void exit(int status);
+bool create(const char *file, unsigned initial_size);
+bool remove(const char *file);
+void check_address(void *addr);
+void get_argument(void *rsp, int *arg, int count);
+
 void syscall_init(void)
 {
 	write_msr(MSR_STAR, ((uint64_t)SEL_UCSEG - 0x10) << 48 |
@@ -40,17 +55,50 @@ void syscall_init(void)
 }
 
 /* The main system call interface */
-void syscall_handler(struct intr_frame *f UNUSED)
+void syscall_handler(struct intr_frame *f)
 {
-	// TODO: [Part2] Your implementation goes here.
+	// NOTE: [Part2] Your implementation goes here.
 	/* 유저 스택에 저장되어 있는 시스템 콜 넘버를 이용해 시스템 콜 핸들러 구현*/
 	/* 스택 포인터가 유저 영역인지 확인 */
 	/* 저장된 인자 값이 포인터일 경우 유저 영역의 주소인지 확인 */
 	/* 0 : halt */
 	/* 1 : exit */
 	/* . . . */
-	printf("system call!\n");
-	thread_exit();
+
+	void *rsp = f->rsp;
+	check_address(rsp);
+	int syscall_num = *(int *)rsp;
+	rsp += sizeof(int *);
+
+	int *argv = NULL;
+	bool result;
+
+	switch (syscall_num)
+	{
+	case SYS_HALT:
+		halt();
+		break;
+	case SYS_EXIT:
+		argv = realloc(argv, sizeof(int));
+		get_argument(&rsp, argv, 1);
+		exit(argv[0]);
+		break;
+	case SYS_CREATE:
+		argv = realloc(argv, sizeof(int) * 2);
+		get_argument(&rsp, argv, 2);
+		result = create(argv[0], argv[1]);
+		memcpy(f->R.rax, &result, sizeof(bool));
+		break;
+	case SYS_REMOVE:
+		argv = realloc(argv, sizeof(int));
+		get_argument(&rsp, argv, 1);
+		result = remove(argv[0]);
+		memcpy(f->R.rax, &result, sizeof(bool));
+		break;
+	default:
+		thread_exit();
+		break;
+	}
 }
 
 /* NOTE: [Part2] pintos를 종료시키는 시스템 콜 */
@@ -60,45 +108,60 @@ void halt(void)
 	power_off();
 }
 
-/* TODO: [Part2] 현재 프로세스를 종료시키는 시스템 콜 */
+/* NOTE: [Part2] 현재 프로세스를 종료시키는 시스템 콜 */
 void exit(int status)
 {
-	/**
-	 * 실행 중인 스레드 구조체를 가져옴
-	 * 프로세스 종료 메세지 출력,
-	 * 출력 양식 : "프로세스이름: exit(종료상태)"
-	 * 스레드 종료
-	 */
+	/* 실행중인 스레드 구조체를 가져옴 */
+	struct thread *curr = thread_current();
+	/* 프로세스 종료 메시지 출력,
+	출력 양식: “프로세스이름 : exit(종료상태 )” */
+	printf("%s : exit(%d)\n", curr->name, status);
+	/* 스레드 종료 */
+	thread_exit();
 }
 
-/* TODO: [Part2] 파일을 생성하는 시스템 콜*/
+/* NOTE: [Part2] 파일을 생성하는 시스템 콜*/
 bool create(const char *file, unsigned initial_size)
 {
-	/**
-	 * 파일 이름과 크기에 해당하는 파일 생성
-	 * 파일 생성 성공 시 true 반환, 실패 시 false 반환
-	 */
+	bool success;
+	/* 파일 이름과 크기에 해당하는 파일 생성*/
+	success = filesys_create(file, initial_size);
+	/* 파일 생성 성공 시 true 반환, 실패 시 false 반환 */
+	return success;
 }
 
-/* TODO: [Part2] 파일을 삭제하는 시스템 콜 */
+/* NOTE: [Part2] 파일을 삭제하는 시스템 콜 */
 bool remove(const char *file)
 {
-	/**
-	 * 파일 이름에 해당하는 파일 제거
-	 * 파일 제거 성공 시 true 반환, 실패 시 false 반환
-	 */
+	bool success;
+	/* 파일 이름에 해당하는 파일을 제거*/
+	success = filesys_remove(file);
+	/* 파일 제거 성공 시 true 반환, 실패 시 false 반환 */
+	return success;
 }
 
-/* TODO: [Part2] 추가 함수 - 주소 값이 유저 영역에서 사용하는 주소 값인지 확인하는 함수 */
+/* NOTE: [Part2] 추가 함수 - 주소 값이 유저 영역에서 사용하는 주소 값인지 확인하는 함수 */
 void check_address(void *addr)
 {
 	/* 포인터가 가리키는 주소가 유저영역의 주소인지 확인*/
-	/* 잘못된 접근일 경우 프로세스 종료 */
+	if (addr < USER_AREA_STAR || addr > USER_AREA_END)
+	{
+		/* 잘못된 접근일 경우 프로세스 종료 */
+		exit(-1);
+	}
 }
 
-/* TODO: [Part2] 추가 함수 - 유저 스택에 있는 인자들을 커널에 저장하는 함수 */
+/* NOTE: [Part2] 추가 함수 - 유저 스택에 있는 인자들을 커널에 저장하는 함수 */
 void get_argument(void *rsp, int *arg, int count)
 {
 	/* 유저 스택에 저장된 인자값들을 커널로 저장*/
-	/* 인자가 저장된 위치가 유저영역인지 확인 */
+	for (int i = 0; i < count; i++)
+	{
+		/* 인자가 저장된 위치가 유저영역인지 확인 */
+		check_address(rsp);
+
+		/* 인자를 커널로 복사 */
+		memcpy(&arg[i], rsp, sizeof(int *));
+		rsp += sizeof(int *);
+	}
 }
